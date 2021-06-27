5dd679f258c472f5bb145008d8d20336c617ff4e
Daniil <baranov2394@gmail.com>
2021-06-27 19:30:45 +0300

    add new text

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,67 +1,10 @@
-<section class="footer">
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<div class="column-footer">
-							<div class="column-footer__title">
-								<h4>Юридический адрес</h4>
-							</div>
-							<div class="column-footer__text">
-								<p class="column-footer__text-address">г.Санкт-Петерубрг д.52</p>
-								<div class="column-footer__text-telephone">Тел. +7 (911) 111-11-11</div>
-							</div>
-							<div class="column-footer__title">
-								<h4>Издательский отдел</h4>
-							</div>
-							<div class="column-footer__text">
-								<div class="column-footer__text-email">baranov@gmail.com</div>
-							</div>
-						</div>
-					</div>
-					<div class="col">
-						<div class="column-footer">
-							<div class="column-footer__title">
-								<h4>Помощь</h4>
-							</div>
-							<ul class="column-footer__navigation">
-								<li><a href="index.php" class="navigation__link">Главная</a></li>
-								<li><a href="instraction.php#" class="navigation__link">Руководство</a></li>
-								<li><a href="year.php?year_id=1" class="navigation__link">Каталог</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="col">
-						<div class="column-footer">
-							<div class="socials">
-								<a href="#" class="socials__item">
-									<i class="fab fa-vk none"></i>
-								</a>
-								<a href="#" class="socials__item">
-									<i class="fab fa-twitter"></i>
-								</a>
-								<a href="#" class="socials__item">
-									<i class="fab fa-facebook-f"></i>
-								</a>
-								<a href="#" class="socials__item">
-									<i class="fab fa-instagram"></i>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	</section>
-	
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-		integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-		crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-		integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-		crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-		integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-		crossorigin="anonymous"></script>
-		</body>
-		</html>
+<footer class="container">
+    <p class="float-end"><a href="#">Back to top</a></p>
+    <p>© 2017–2021 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+  </footer>
+  </main>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
--- blocks/head.php
+++ blocks/head.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title><?= $website_title ?></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 <!--Нормализация стилей-->
 <link rel="stylesheet" href="css/normalize.css">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
-<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="css/font-awesome.min.css">
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-
 <link rel="shortcut icon" href="img/favicon.ico">
-
 <script type="text/javascript" src="js/main.js"></script>
 </head>
 <body>
-
